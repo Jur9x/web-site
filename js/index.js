b046ca93be66731e0c641131e3b30f5e468110a0
Jiří Ryba <jirka.ryba02@gmail.com>
2021-11-09 19:47:49 +0100

    2.0 Roztřídění, úprava js, html

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/*var number = 0;
+
+function disco() {
+    if (number == 0) {
+        document.getElementByClassName('havr').classList.add('disco');
+        return number = 1;
+    }
+    if (number == 1) {
+        document.getElementByClassName('havr').classList.remove('disco');
+        return number = 0;
+    }
+}*/
